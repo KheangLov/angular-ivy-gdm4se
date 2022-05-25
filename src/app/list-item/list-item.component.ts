bec6b1e298a8a17e171c0b28b7dfb7a8a66df89b
KheangLov <lovsokheang@gmail.com>
2022-05-25 16:05:00 +0700

    2 files modified

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { filter } from 'rxjs/operators';
 
 import { FormDialogComponent } from '../form-dialog/form-dialog.component';
 import { BookService } from '../book.service';
@@ -11,11 +12,16 @@ import { IBook } from '../type-interface';
   styleUrls: ['./list-item.component.css'],
 })
 export class ListItemComponent implements OnInit {
+  bookData: IBook;
   bookList: Array<IBook> = [];
   constructor(private bookService: BookService, public dialog: MatDialog) {}
 
   ngOnInit() {
-    this.bookService.getBooks().subscribe((books) => (this.bookList = books));
+    this.bookService
+      .getBooks()
+      .subscribe(
+        (books) => (this.bookList = books.filter((book) => book.is_active))
+      );
   }
 
   openDialog(data = {}) {
@@ -32,5 +38,19 @@ export class ListItemComponent implements OnInit {
         (book) =>
           (this.bookList = this.bookList.filter((data) => data.id !== book.id))
       );
+  }
+
+  handleBookView(book: IBook) {
+    this.bookData = book;
+  }
+
+  clearData() {
+    this.bookData = {
+      id: null,
+      title: null,
+      category: null,
+      createdAt: null,
+      is_active: null,
+    };
   }
 }
