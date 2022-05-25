4674c163ae366321896641c773547d2b93c363fb
KheangLov <lovsokheang@gmail.com>
2022-05-25 11:35:17 +0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface ICategory {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  categories: Array<ICategory> = [
+    { value: 'Technology', viewValue: 'Technology' },
+    { value: 'Cartoon', viewValue: 'Cartoon' },
+    { value: 'General', viewValue: 'General' },
+  ];
+  statuses = ['Inactive', 'Active'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatRadioModule,
+  ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
