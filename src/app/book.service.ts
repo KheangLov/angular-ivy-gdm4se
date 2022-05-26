288d100a75ce7e84ddb75339b96daf602f599d6c
KheangLov <lovsokheang@gmail.com>
2022-05-26 08:09:35 +0700

    1 file modified

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -14,33 +14,27 @@ const initialHttpOption = {
   providedIn: 'root',
 })
 export class BookService {
+  apiUrl: string =
+    'https://628da730368687f3e706c47f.mockapi.io/api/angular-mat/books';
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<IBook[]> {
-    return this.http.get<IBook[]>(
-      'https://628da730368687f3e706c47f.mockapi.io/api/angular-mat/books'
-    );
+    return this.http.get<IBook[]>(this.apiUrl);
   }
 
   createBook(book: IBookForm): Observable<IBook> {
-    return this.http.post<IBook>(
-      'https://628da730368687f3e706c47f.mockapi.io/api/angular-mat/books',
-      book,
-      initialHttpOption
-    );
+    return this.http.post<IBook>(this.apiUrl, book, initialHttpOption);
   }
 
   updateBook(book: IBook): Observable<IBook> {
     return this.http.put<IBook>(
-      `https://628da730368687f3e706c47f.mockapi.io/api/angular-mat/books/${book.id}`,
+      `${this.apiUrl}/${book.id}`,
       book,
       initialHttpOption
     );
   }
 
   deleteBook(book: IBook): Observable<IBook> {
-    return this.http.delete<IBook>(
-      `https://628da730368687f3e706c47f.mockapi.io/api/angular-mat/books/${book.id}`
-    );
+    return this.http.delete<IBook>(`${this.apiUrl}/${book.id}`);
   }
 }
